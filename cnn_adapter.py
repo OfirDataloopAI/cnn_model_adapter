658f31cedb372eab7f1cda56c24e0698ee10638a
OfirDataloopAI <ofir.g@dataloop.ai>
2023-05-08 12:34:44 +0300

    fresh upload

---
+++ cnn_adapter.py
@@ -0,0 +1,234 @@
+# from torch.utils.data import DataLoader
+from imgaug import augmenters as iaa
+import torchvision.transforms
+# import torch.optim as optim
+import torch.nn.functional
+# import pandas as pd
+# import numpy as np
+import dtlpy as dl
+import torch.nn
+import logging
+import torch
+# import time
+# import copy
+# import tqdm
+import os
+
+from torch.utils.data import DataLoader
+from dtlpy.utilities.dataset_generators.dataset_generator import collate_torch
+from dtlpy.utilities.dataset_generators.dataset_generator_torch import DatasetGeneratorTorch
+
+import cnn_model
+
+logger = logging.getLogger('cnn-adapter')
+
+
+@dl.Package.decorators.module(name='model-adapter',
+                              description='Model Adapter for Dummy Model',
+                              init_inputs={'model_entity': dl.Model})
+class ModelAdapter(dl.BaseModelAdapter):
+    """
+    CNN adapter using pytorch.
+    The class bind Dataloop model and model entities with model code implementation
+    """
+
+    def __init__(self, model_entity=None):
+        if not isinstance(model_entity, dl.Model):
+            if isinstance(model_entity, str):
+                model_entity = dl.models.get(model_id=model_entity)
+            if isinstance(model_entity, dict) and 'model_id' in model_entity:
+                model_entity = dl.models.get(model_id=model_entity['model_id'])
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        # TODO: GET MODEL
+        self.model = cnn_model.CNN(use_dropout=True).to(self.device)
+        super(ModelAdapter, self).__init__(model_entity=model_entity)
+
+    def load(self, local_path: str, **kwargs):
+        weights = self.model_entity.configuration.get('weights', 'model.pth')
+
+        # TODO: LOAD MODEL
+        self.model.load_state_dict(torch.load(weights))
+        logger.info("Model loaded successfully")
+
+    def save(self, local_path: str, **kwargs):
+
+        # TODO: SAVE MODEL
+        weights = kwargs.get('weights', 'model.pth')
+        torch.save(self.model, os.path.join(local_path, weights))
+        self.configuration['weights'] = weights
+        logger.info("Model saved successfully")
+
+    def train(self, data_path: str, output_path: str, **kwargs):
+        ######################
+        # Create Dataloaders #
+        ######################
+        input_size = self.configuration.get('input_size', 256)
+
+        def gray_to_rgb(x):
+            return x.convert('RGB')
+
+        data_transforms = {
+            'train': [
+                iaa.Resize({"height": input_size, "width": input_size}),
+                iaa.flip.Fliplr(p=0.5),
+                iaa.flip.Flipud(p=0.2),
+                torchvision.transforms.ToPILImage(),
+                gray_to_rgb,
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ],
+            'valid': [
+                torchvision.transforms.ToPILImage(),
+                gray_to_rgb,
+                torchvision.transforms.Resize((input_size, input_size)),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ]
+        }
+
+        train_dataset = DatasetGeneratorTorch(data_path=os.path.join(data_path, 'train'),
+                                              dataset_entity=self.model_entity.dataset,
+                                              annotation_type=dl.AnnotationType.CLASSIFICATION,
+                                              transforms=data_transforms['train'],
+                                              id_to_label_map=self.model_entity.id_to_label_map,
+                                              class_balancing=True)
+
+        valid_dataset = DatasetGeneratorTorch(data_path=os.path.join(data_path, 'validation'),
+                                              dataset_entity=self.model_entity.dataset,
+                                              annotation_type=dl.AnnotationType.CLASSIFICATION,
+                                              transforms=data_transforms['valid'],
+                                              id_to_label_map=self.model_entity.id_to_label_map)
+
+        batch_size = self.configuration.get("batch_size", 16)
+        dataloaders = {'train': DataLoader(train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True,
+                                           collate_fn=collate_torch),
+                       'valid': DataLoader(valid_dataset,
+                                           batch_size=batch_size,
+                                           collate_fn=collate_torch,
+                                           shuffle=True)}
+
+        # TODO: TRAIN MODEL
+        logger.info("Model started training")
+        hyper_parameters = self.configuration.get('hyper_parameters', None)
+        self.model = cnn_model.CNN(output_size=hyper_parameters.get("output_size", 10),
+                                   use_dropout=True).to(self.device)
+        cnn_model.train_model(model=self.model,
+                              device=self.device,
+                              hyper_parameters=hyper_parameters,
+                              dataloaders=dataloaders,
+                              output_path=output_path)
+
+        logger.info("Model trained successfully")
+
+    def predict(self, batch: list, **kwargs):
+        # TODO: PREDICT MODEL
+        input_size = self.configuration.get('input_size', 256)
+        batch_predictions = cnn_model.predict(model=self.model, device=self.device, batch=batch, input_size=input_size)
+        batch_annotations = list()
+
+        for img_prediction in batch_predictions:
+            pred_score, high_pred_index = torch.max(img_prediction, 0)
+            pred_label = self.model_entity.id_to_label_map.get(int(high_pred_index.item()), 'UNKNOWN')
+            collection = dl.AnnotationCollection()
+            collection.add(annotation_definition=dl.Classification(label=pred_label),
+                           model_info={'name': self.model_entity.name,
+                                       'confidence': pred_score.item(),
+                                       'model_id': self.model_entity.id,
+                                       'dataset_id': self.model_entity.dataset_id})
+            logger.debug("Predicted {:20} ({:1.3f})".format(pred_label, pred_score))
+            batch_annotations.append(collection)
+
+        return batch_annotations
+
+    def convert_from_dtlpy(self, data_path, **kwargs):
+        """ Convert Dataloop structure data to model structured
+            Virtual method - need to implement
+            e.g. take dlp dir structure and construct annotation file
+        :param data_path: `str` local File System directory path where
+                           we already downloaded the data from dataloop platform
+        :return:
+        """
+
+        ...
+
+
+def package_creation(project: dl.Project):
+    metadata = dl.Package.get_ml_metadata(cls=ModelAdapter,
+                                          default_configuration={'weights': 'model.pth',
+                                                                 'input_size': 256,
+                                                                 'hyper_parameters': {
+                                                                     "num_epochs": 50,
+                                                                     "optimizer_lr": 0.01,
+                                                                     "output_size": 10,
+                                                                 }},
+                                          output_type=dl.AnnotationType.POINT,
+                                          )
+    module = dl.PackageModule.from_entry_point(entry_point='cnn_adapter.py')
+    package = project.packages.push(package_name='cnn',
+                                    src_path=os.getcwd(),
+                                    # description='CNN implemented in pytorch',
+                                    is_global=True,
+                                    package_type='ml',
+                                    codebase=dl.GitCodebase(
+                                        type=dl.PackageCodebaseType.GIT,
+                                        git_url='https://github.com/OfirDataloopAI/cnn_model_adapter',
+                                        git_tag='v1'),
+                                    modules=[module],
+                                    service_config={
+                                        'runtime': dl.KubernetesRuntime(pod_type=dl.INSTANCE_CATALOG_HIGHMEM_L,
+                                                                        runner_image='gcr.io/viewo-g/modelmgmt/resnet:0.0.7',
+                                                                        autoscaler=dl.KubernetesRabbitmqAutoscaler(
+                                                                            min_replicas=0,
+                                                                            max_replicas=1),
+                                                                        concurrency=1).to_json()},
+                                    metadata=metadata)
+    # package.metadata = {'system': {'ml': {'defaultConfiguration': {'weights': 'model.pth',
+    #                                                                'input_size': 256},
+    #                                       'outputType': dl.AnnotationType.CLASSIFICATION,
+    #                                       'tags': ['torch'], }}}
+    # package = package.update()
+    # s = package.services.list().items[0]
+    # s.package_revision = package.version
+    # s.versions['dtlpy'] = '1.74.9'
+    # s.update(True)
+    return package
+
+
+def model_creation(package: dl.Package, project: dl.Project):
+    labels = list()
+    for i in range(10):
+        labels.append(str(i))
+
+    model = package.models.create(model_name='dummy-model',
+                                  description='dummy-model for testing',
+                                  tags=['pretrained', 'no-data'],
+                                  dataset_id=None,
+                                  scope='public',
+                                  status='trained',
+                                  configuration={'weights': 'model.pth',
+                                                 'batch_size': 16,
+                                                 'hyper_parameters': {
+                                                     "num_epochs": 50,
+                                                     "optimizer_lr": 0.01,
+                                                     "output_size": 10,
+                                                 }},
+                                  project_id=project.id,
+                                  labels=labels,
+                                  )
+    return model
+
+
+if __name__ == "__main__":
+    dl.setenv('rc')
+    project = dl.projects.get(project_name='QA_MODELS_V3')
+    package_creation(project=project)
+    package = project.packages.get(package_name='cnn')
+    # package.artifacts.list()
+    model_creation(package=package, project=project)
+
+    # Useful:
+    #https://github.com/dataloop-ai/pytorch_adapters/blob/mgmt3/resnet_adapter.py
---
+++ cnn_model.py
@@ -0,0 +1,286 @@
+# import numpy as np
+import torch
+import torchvision
+# import copy
+
+# import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+# import torchvision.transforms as T
+
+# from dtlpy.utilities.dataset_generators.dataset_generator_torch import DatasetGeneratorTorch
+
+
+# from torch.autograd import Variable
+# from torch.utils.data.sampler import SubsetRandomSampler
+# from torchvision import models
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+from tqdm import tqdm
+# from imgaug import augmenters as iaa
+# import os
+
+### Setting device
+
+# if torch.cuda.is_available():
+#     device = torch.device("cuda")
+# else:
+#     device = torch.device("cpu")
+#
+# print("Using device:", device)
+#
+# ### Prepare dataloaders
+#
+# transform = T.Compose([
+#     T.ToTensor(),
+#     T.Normalize((0.5,), (0.5,))
+# ])
+#
+# # Number of Training images
+# N = 30000
+# batch_size = 128
+#
+# # Datasets
+# trainset = torchvision.datasets.MNIST(root='./data', train=True,
+#                                       download=True, transform=transform)
+# testset = torchvision.datasets.MNIST(root='./data', train=False,
+#                                      download=True, transform=transform)
+#
+# # Train and Validation split
+# train_size = len(trainset)
+# train_idx = np.arange(train_size)
+# train_subset_idx = np.random.choice(train_idx, N)
+# train_subset_idx, val_subset_idx = train_test_split(train_subset_idx,
+#                                                     test_size=0.2,
+#                                                     random_state=0)
+#
+# # Create samplers
+# train_sampler = SubsetRandomSampler(train_subset_idx)
+# validaiton_sampler = SubsetRandomSampler(val_subset_idx)
+#
+#
+# mean = [0.4914, 0.4822, 0.4465]
+# standard_deviation = [0.2471, 0.2435, 0.2616]
+#
+#
+# # Training Loaders
+# trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+#                                           sampler=train_sampler, num_workers=2)
+# validationloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+#                                                sampler=validaiton_sampler, num_workers=2)
+#
+# # MNIST Testing Loader
+# testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+#                                          shuffle=True, num_workers=2)
+#
+#
+# trainset_size = len(train_subset_idx)
+# validset_size = len(val_subset_idx)
+# testset_size = len(testset)
+
+
+# print('='*25)
+# print('Train dataset:', trainset_size)
+# print('Validation dataset:', validset_size)
+# print('Test dataset:', testset_size)
+# print('='*25)
+
+
+# Model define
+class CNN(nn.Module):
+    def __init__(self, output_size=10, use_dropout=False, use_dropout2d=False):
+        super(CNN, self).__init__()
+
+        self.output_size = output_size
+        # flags
+        self.use_dropout = use_dropout
+        self.use_dropout2d = use_dropout2d
+        # kernel
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, padding='same')
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding='same')
+        if self.use_dropout2d:
+            self.spatial_dropout = nn.Dropout2d(p=0.2)
+        # FC layers - since we use global avg pooling,
+        # input to the FC layer = #output_features of the second conv layer
+        self.fc1 = nn.Linear(32, 256)
+        if self.use_dropout:
+            self.dropout = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(256, output_size)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # adaptive_avg_pool2d with output_size=1 = simple global avg pooling
+        x = self.conv2(x)
+        if self.use_dropout2d:
+            x = self.spatial_dropout(x)
+        x = F.relu(x)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = torch.flatten(x, 1)  # flatten all dimensions except the batch dimension
+        x = F.relu(self.fc1(x))
+        if self.use_dropout:
+            x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+
+def train_model(model: CNN, device, hyper_parameters, dataloaders, output_path):
+    #########################
+    # Load Hyper Parameters #
+    #########################
+    num_epochs = hyper_parameters.get("num_epochs", 50)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=hyper_parameters.get("optimizer_lr", 0.01))
+
+    x_axis = list(range(num_epochs))
+    CNN_graph_data = dict()
+    CNN_graph_data["train"] = dict()
+    CNN_graph_data["valid"] = dict()
+    CNN_graph_data["train"]["loss"] = list()
+    CNN_graph_data["valid"]["loss"] = list()
+    CNN_graph_data["train"]["accuracy"] = list()
+    CNN_graph_data["valid"]["accuracy"] = list()
+
+    ######################
+    # Create Dataloaders #
+    ######################
+
+
+    optimal_val_epoch = 0
+    optimal_val_accuracy = 0
+
+    for epoch in tqdm(range(num_epochs)):
+        # Each epoch has a training and validation phase
+        for phase in ["train", "valid"]:
+            if phase == "train":
+                # Set model to training mode
+                model.train()
+
+                dataloader = dataloaders["train"]
+                dataset_size = len(dataloader.dataset)
+            else:
+                # Set model to evaluate mode
+                model.eval()
+                dataloader = dataloaders["valid"]
+                dataset_size = len(dataloader.dataset)
+
+            running_loss = 0.0
+            running_corrects = 0.0
+
+            for i, data in enumerate(dataloader, start=0):
+                inputs, labels = data
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the gradient
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == "train"):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # Calculating backward and optimize only during the training phase
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+
+                # Total loss of the mini batch
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data).cpu()
+
+            epoch_loss = running_loss / dataset_size
+            epoch_accuracy = (running_corrects / dataset_size) * 100
+            CNN_graph_data[phase]["loss"].append(epoch_loss)
+            CNN_graph_data[phase]["accuracy"].append(epoch_accuracy)
+
+    return epoch_loss, epoch_accuracy
+
+
+def predict(model: CNN, device, batch, input_size):
+    preprocess = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.ToPILImage(),
+            torchvision.transforms.Resize(input_size),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # [-1, 1]
+        ]
+    )
+    img_tensors = [preprocess(img.astype('uint8')) for img in batch]
+    batch_tensor = torch.stack(tensors=img_tensors).to(device)
+    batch_output = model(batch_tensor)
+    batch_predictions = nn.functional.softmax(batch_output, dim=1)
+
+    return batch_predictions
+
+
+
+
+
+# def validate_model(model, dataloader, criterion):
+#     model.eval()  # Set model to evaluate mode
+#     running_loss = 0
+#     running_corrects = 0
+#
+#     with torch.no_grad():
+#         for i, data in enumerate(dataloader, 0):
+#             # get the inputs; data is a list of [inputs, labels]
+#             inputs, labels = data
+#             # mode to device/cuda
+#             inputs, labels = inputs.to(device), labels.to(device)
+#             logits = model(inputs)
+#             _, preds = torch.max(logits, 1)
+#
+#             # Cross-entropy loss
+#             loss = criterion(logits, labels)
+#
+#             # statistics
+#             running_loss += loss.item() * inputs.size(0)
+#             running_corrects += torch.sum(preds == labels.data)
+#
+#     epoch_loss = running_loss / validset_size
+#     epoch_accuracy = ((running_corrects.double() / validset_size) * 100).item()
+#
+#     return epoch_loss, epoch_accuracy
+#
+#
+# for epoch in tqdm(range(num_epochs)):
+#     train_loss, train_accuracy = train_epoch(CNN_model, trainloader, CNN_criterion, CNN_optimizer)
+#     val_loss, val_accuracy = val_epoch(CNN_model, validationloader, CNN_criterion)
+#
+#     CNN_graph_data["train"]["loss"].append(train_loss)
+#     CNN_graph_data["valid"]["loss"].append(val_loss)
+#     CNN_graph_data["train"]["accuracy"].append(train_accuracy)
+#     CNN_graph_data["valid"]["accuracy"].append(val_accuracy)
+#
+#     if val_accuracy > optimal_val_accuracy:
+#         optimal_val_epoch = epoch
+#         optimal_val_accuracy = val_accuracy
+#         PATH = "./cnn_parameters.pth"
+#         torch.save(copy.deepcopy(CNN_model.state_dict()), PATH)
+#
+#
+# plt.title("CNN Loss:")
+# plt.plot(x_axis, CNN_graph_data["train"]["loss"], label="Train")
+# plt.plot(x_axis, CNN_graph_data["valid"]["loss"], label="Validation")
+# plt.xlabel("Number of epochs")
+# plt.ylabel("Loss")
+# plt.legend()
+# plt.show()
+#
+# plt.title("CNN Accuracy:")
+# plt.plot(x_axis, CNN_graph_data["train"]["accuracy"], label="Train")
+# plt.plot(x_axis, CNN_graph_data["valid"]["accuracy"], label="Validation")
+# plt.xlabel("Number of epochs")
+# plt.ylabel("Accuracy")
+# plt.legend()
+# plt.show()
+#
+# print("Optimal hyper parameters were found at:")
+# print("Epoch:", optimal_val_epoch)
+# print("The Validation Accuracy:", optimal_val_accuracy)
+#
+# PATH = "./cnn_parameters.pth"
+# CNN_model.load_state_dict(torch.load(PATH))
