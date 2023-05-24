22126a0bf78ba2f4bfd4c4c60c6ffb221566cb1b
OfirDataloopAI <ofir.g@dataloop.ai>
2023-05-24 09:56:18 +0300

    in progress

--- cnn_adapter.py
+++ cnn_adapter.py
@@ -23,9 +23,9 @@ import cnn_model
 logger = logging.getLogger('cnn-adapter')
 
 
-@dl.Package.decorators.module(name='model-adapter',
-                              description='CNN Adapter for CNN Example Model',
-                              init_inputs={'model_entity': dl.Model})
+@dl.Package.decorators.module(name="model-adapter",
+                              description="CNN Adapter for CNN Example Model",
+                              init_inputs={"model_entity": dl.Model})
 class ModelAdapter(dl.BaseModelAdapter):
     """
     CNN adapter using pytorch.
@@ -36,8 +36,8 @@ class ModelAdapter(dl.BaseModelAdapter):
         if not isinstance(model_entity, dl.Model):
             if isinstance(model_entity, str):
                 model_entity = dl.models.get(model_id=model_entity)
-            if isinstance(model_entity, dict) and 'model_id' in model_entity:
-                model_entity = dl.models.get(model_id=model_entity['model_id'])
+            if isinstance(model_entity, dict) and "model_id" in model_entity:
+                model_entity = dl.models.get(model_id=model_entity["model_id"])
 
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -47,7 +47,7 @@ class ModelAdapter(dl.BaseModelAdapter):
         logger.info("Model init completed")
 
     def load(self, local_path: str, **kwargs):
-        weights_filename = self.model_entity.configuration.get('weights_filename', 'model.pth')
+        weights_filename = self.model_entity.configuration.get("weights_filename", "model.pth")
 
         # TODO: LOAD MODEL - CURRENTLY WORK WITH GPU ONLY
         if not self.model:
@@ -59,9 +59,9 @@ class ModelAdapter(dl.BaseModelAdapter):
 
     def save(self, local_path: str, **kwargs):
         # TODO: SAVE MODEL
-        weights_filename = kwargs.get('weights_filename', 'model.pth')
+        weights_filename = kwargs.get("weights_filename", "model.pth")
         torch.save(self.model, os.path.join(local_path, weights_filename))
-        self.configuration['weights_filename'] = weights_filename
+        self.configuration["weights_filename"] = weights_filename
         logger.info("Model saved successfully")
 
     def train(self, data_path: str, output_path: str, **kwargs):
@@ -72,36 +72,36 @@ class ModelAdapter(dl.BaseModelAdapter):
         ######################
         # Create Dataloaders #
         ######################
-        input_size = self.configuration.get('input_size', 28)
+        input_size = self.configuration.get("input_size", 28)
         data_transforms = cnn_model.get_data_transforms(input_size=input_size)
 
-        train_dataset = DatasetGeneratorTorch(data_path=os.path.join(data_path, 'train'),
+        train_dataset = DatasetGeneratorTorch(data_path=os.path.join(data_path, "train"),
                                               dataset_entity=self.model_entity.dataset,
                                               annotation_type=dl.AnnotationType.CLASSIFICATION,
-                                              transforms=data_transforms['train'],
+                                              transforms=data_transforms["train"],
                                               id_to_label_map=self.model_entity.id_to_label_map,
                                               class_balancing=True)
 
-        valid_dataset = DatasetGeneratorTorch(data_path=os.path.join(data_path, 'validation'),
+        valid_dataset = DatasetGeneratorTorch(data_path=os.path.join(data_path, "validation"),
                                               dataset_entity=self.model_entity.dataset,
                                               annotation_type=dl.AnnotationType.CLASSIFICATION,
-                                              transforms=data_transforms['valid'],
+                                              transforms=data_transforms["valid"],
                                               id_to_label_map=self.model_entity.id_to_label_map)
 
         batch_size = self.configuration.get("batch_size", 16)
-        dataloaders = {'train': DataLoader(dataset=train_dataset,
+        dataloaders = {"train": DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True,
                                            collate_fn=collate_torch),
-                       'valid': DataLoader(dataset=valid_dataset,
+                       "valid": DataLoader(dataset=valid_dataset,
                                            batch_size=batch_size,
                                            collate_fn=collate_torch,
                                            shuffle=True)}
 
         # TODO: TRAIN MODEL
         logger.info("Model started training")
-        hyper_parameters = self.configuration.get('hyper_parameters', None)
-        labels_count = self.configuration.get('id_to_label_map', None)
+        hyper_parameters = self.configuration.get("hyper_parameters", None)
+        labels_count = self.configuration.get("id_to_label_map", None)
         if labels_count:
             output_size = len(labels_count)
         else:
@@ -170,19 +170,19 @@ class ModelAdapter(dl.BaseModelAdapter):
             self.model = cnn_model.CNN(use_dropout=True).to(self.device)
 
         # TODO: PREDICT MODEL
-        input_size = self.configuration.get('input_size', 28)
+        input_size = self.configuration.get("input_size", 28)
         batch_predictions = cnn_model.predict(model=self.model, device=self.device, batch=batch, input_size=input_size)
         batch_annotations = list()
 
         for img_prediction in batch_predictions:
             pred_score, high_pred_index = torch.max(img_prediction, 0)
-            pred_label = self.model_entity.id_to_label_map.get(int(high_pred_index.item()), 'UNKNOWN')
+            pred_label = self.model_entity.id_to_label_map.get(int(high_pred_index.item()), "UNKNOWN")
             collection = dl.AnnotationCollection()
             collection.add(annotation_definition=dl.Classification(label=pred_label),
-                           model_info={'name': self.model_entity.name,
-                                       'confidence': pred_score.item(),
-                                       'model_id': self.model_entity.id,
-                                       'dataset_id': self.model_entity.dataset_id})
+                           model_info={"name": self.model_entity.name,
+                                       "confidence": pred_score.item(),
+                                       "model_id": self.model_entity.id,
+                                       "dataset_id": self.model_entity.dataset_id})
             logger.debug("Predicted {:1} ({:1.3f})".format(pred_label, pred_score))
             batch_annotations.append(collection)
 
@@ -204,39 +204,53 @@ class ModelAdapter(dl.BaseModelAdapter):
 # Package Creation #
 ####################
 def package_creation(project: dl.Project):
+    weights_filename_path = "model.pth"
+    batch_size = 16
+    input_size = 28
+    num_epochs = 50
+    optimizer_lr = 0.01
+    output_size = 10
+
+    # Package Metadata
     metadata = dl.Package.get_ml_metadata(
         cls=ModelAdapter,
         default_configuration={
-            "weights_filename": "model.pth",
-            "batch_size": 16,
-            "input_size": 28,
+            "weights_filename": weights_filename_path,
+            "batch_size": batch_size,
+            "input_size": input_size,
             "hyper_parameters": {
-                "num_epochs": 50,
-                "optimizer_lr": 0.01,
-                "output_size": 10,
+                "num_epochs": num_epochs,
+                "optimizer_lr": optimizer_lr,
+                "output_size": output_size,
             }
         },
         output_type=dl.AnnotationType.CLASSIFICATION,
     )
 
+    package_name = "cnn"
+    git_url = "https://github.com/OfirDataloopAI/cnn_model_adapter"
     # TODO: Very important to add tag
-    module = dl.PackageModule.from_entry_point(entry_point='cnn_adapter.py')
+    git_tag = "v13"
+    # TODO: check different image
+    docker_image = "gcr.io/viewo-g/modelmgmt/resnet:0.0.7"
+    module = dl.PackageModule.from_entry_point(entry_point="cnn_adapter.py")
+
+    # Package Creation
     package = project.packages.push(
-        package_name='cnn',
+        package_name=package_name,
         src_path=os.getcwd(),
         is_global=False,
-        package_type='ml',
+        package_type="ml",
         codebase=dl.GitCodebase(
             type=dl.PackageCodebaseType.GIT,
-            git_url='https://github.com/OfirDataloopAI/cnn_model_adapter',
-            git_tag='v13'
+            git_url=git_url,
+            git_tag=git_tag
         ),
         modules=[module],
         service_config={
-            'runtime': dl.KubernetesRuntime(
+            "runtime": dl.KubernetesRuntime(
                 pod_type=dl.INSTANCE_CATALOG_HIGHMEM_L,
-                #TODO: check different image
-                runner_image='gcr.io/viewo-g/modelmgmt/resnet:0.0.7',
+                runner_image=docker_image,
                 autoscaler=dl.KubernetesRabbitmqAutoscaler(
                     min_replicas=0,
                     max_replicas=1
@@ -247,15 +261,6 @@ def package_creation(project: dl.Project):
         metadata=metadata
     )
 
-    # package.metadata = {'system': {'ml': {'defaultConfiguration': {'weights_filename': 'model.pth',
-    #                                                                'input_size': 256},
-    #                                       'outputType': dl.AnnotationType.CLASSIFICATION,
-    #                                       'tags': ['torch'], }}}
-    # package = package.update()
-    # s = package.services.list().items[0]
-    # s.package_revision = package.version
-    # s.versions['dtlpy'] = '1.74.9'
-    # s.update(True)
     return package
 
 
@@ -287,7 +292,7 @@ def model_creation(package: dl.Package, project: dl.Project):
     model = package.models.create(
         model_name="cnn",
         description="cnn-model for MNIST dataset",
-        tags=['pretrained', 'MNIST'],
+        tags=["pretrained", "MNIST"],
         dataset_id=dataset.id,
         scope="public",
         status="created",
@@ -311,7 +316,7 @@ def model_creation(package: dl.Package, project: dl.Project):
 
 
 def main():
-    project = dl.projects.get(project_name='Abeer N Ofir Project')
+    project = dl.projects.get(project_name="Abeer N Ofir Project")
 
     # Package Creation
     package_creation(project=project)
